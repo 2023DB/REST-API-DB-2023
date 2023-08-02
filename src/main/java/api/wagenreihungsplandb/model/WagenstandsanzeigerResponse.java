a85ccb3bc66c7916b7b05aa2330fdcc8ecbe774a
2023DB <141234350+2023DB@users.noreply.github.com>
2023-08-02 18:06:02 +0200

    Update WagenstandsanzeigerResponse.java

--- src/main/java/api/wagenreihungsplandb/model/WagenstandsanzeigerResponse.java
+++ src/main/java/api/wagenreihungsplandb/model/WagenstandsanzeigerResponse.java
@@ -1,15 +1,19 @@
 package api.wagenreihungsplandb.model;
 
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import java.util.List;
 
+/**
+ * Represents the response for the Wagenstandsanzeiger application.
+ * Contains the list of Gleisabschnitte (sections) for a train's waggon at a specific station.
+ */
 @Getter
 @Setter
-@ToString
 public class WagenstandsanzeigerResponse {
-    private List<String> sections ;
+    private List<String> sections;
+
+
 
 }
