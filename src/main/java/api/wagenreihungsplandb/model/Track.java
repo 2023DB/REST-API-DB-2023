e2489124a00149d8ef4b210af0d28e320e7f9217
2023DB <141234350+2023DB@users.noreply.github.com>
2023-08-02 18:05:05 +0200

    Update Track.java

--- src/main/java/api/wagenreihungsplandb/model/Track.java
+++ src/main/java/api/wagenreihungsplandb/model/Track.java
@@ -1,26 +1,28 @@
 package api.wagenreihungsplandb.model;
 
-
-
 import lombok.Setter;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import java.util.List;
+
+/**
+ * Represents a track at a station in the WagenreihungsplanDB.
+ */
 @Setter
 public class Track {
 
-    private List<Train> trains ;
+    private List<Train> trains;
+
+    /**
+     * Get the list of trains available on this track.
+     *
+     * @return The list of trains.
+     */
     @XmlElementWrapper(name = "trains")
     @XmlElement(name = "train")
     public List<Train> getTrains() {
         return trains;
     }
 
-    @Override
-    public String toString() {
-        return "Track{" +
-                "trains=" + trains +
-                '}';
-    }
 }
