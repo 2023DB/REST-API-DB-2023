701269eb990d5673f278f0b270d79da9a46aff92
2023DB <141234350+2023DB@users.noreply.github.com>
2023-08-02 18:07:37 +0200

    Update WagenreihungsplanDbApplication.java

--- src/main/java/api/wagenreihungsplandb/WagenreihungsplanDbApplication.java
+++ src/main/java/api/wagenreihungsplandb/WagenreihungsplanDbApplication.java
@@ -3,11 +3,18 @@ package api.wagenreihungsplandb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class to start the WagenreihungsplanDB application.
+ */
 @SpringBootApplication
 public class WagenreihungsplanDbApplication {
 
+	/**
+	 * The entry point of the application.
+	 *
+	 * @param args The command-line arguments passed to the application (not used in this application).
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WagenreihungsplanDbApplication.class, args);
 	}
-
 }
