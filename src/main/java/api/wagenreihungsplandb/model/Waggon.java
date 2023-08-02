1f7a9ee1532c37ef5cfda28db4ba9c39afa5f221
2023DB <141234350+2023DB@users.noreply.github.com>
2023-08-02 18:06:26 +0200

    Update Waggon.java

--- src/main/java/api/wagenreihungsplandb/model/Waggon.java
+++ src/main/java/api/wagenreihungsplandb/model/Waggon.java
@@ -6,27 +6,34 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import java.util.List;
 
+/**
+ * Represents a waggon in a train at a station in the WagenreihungsplanDB.
+ */
 @Setter
 public class Waggon {
 
     private Integer number;
     private List<String> sections;
 
+    /**
+     * Get the number of the waggon.
+     *
+     * @return The number of the waggon.
+     */
     @XmlElement(name = "number")
     public Integer getNumber() {
         return number;
     }
+
+    /**
+     * Get the list of sections (Gleisabschnitte) associated with this waggon.
+     *
+     * @return The list of sections for this waggon.
+     */
     @XmlElementWrapper(name = "sections")
     @XmlElement(name = "identifier")
     public List<String> getSections() {
         return sections;
     }
 
-    @Override
-    public String toString() {
-        return "Waggon{" +
-                "number=" + number +
-                ", sections=" + sections +
-                '}';
-    }
 }
